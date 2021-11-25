b39510d603e1f697d0a9b5000c1238b886bcd1a3
Carlos Usuga Martinez <klich84@gmail.com>
2021-11-25 13:12:49 -0500

    Agragando funcionalidad para eliminar de la base de datos

--- delete.php
+++ delete.php
@@ -1,1 +1,21 @@
-Eliminar
+<?php
+
+	include('database/db.php');
+
+	if (isset($_GET['id'])) {
+		$id = $_GET['id'];
+		// echo $id;
+		$query = "DELETE FROM Tienda WHERE id=$id";
+
+		$result = mysqli_query($conn, $query);
+		// Si no hay un resultado terminanmos la conexion
+		if (!$result) {
+			die('La consulta fallo =(');
+		}
+		$_SESSION['message'] = 'Tarea eliminada satisfactoriamente';
+		$_SESSION['message_type'] = 'danger';
+		// Redireccionamos
+		header('Location: index.php');
+	}
+
+?>
